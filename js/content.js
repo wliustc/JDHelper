72d3c9c81c4244b0ea3b1d0979f3f9fab45cae40
raycdut <raycdut@hotmail.com>
2017-02-25 22:13:39 +0800

    bata

--- js/content.js
+++ js/content.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", myMain, false);
 
 function myMain(evt) {
+
   // DO YOUR STUFF HERE.
   console.log("JD helper running!");
   console.log(window.location.href);
@@ -8,48 +9,14 @@ function myMain(evt) {
 
   if (document.URL === "https://club.jd.com/myJdcomments/myJdcomment.action") {
 
-    $("a[class='btn-def']:contains('评价')").each(function () {
-
-      var href = $(this).attr('href');
-
-     
-      console.log(href);
-
-       window.location.href  = href;
-
-      if (typeof (localStorage) == 'undefined') {
-        alert('Your browser does not support HTML5 localStorage. Try upgrading.');
-      } else {
-
-        var cnt = localStorage.length + 1;
-
-        try {
-          localStorage.setItem('jdhelperurl_' + cnt, href); //saves to the database, “key”, “value”
-        } catch (e) {
-          if (e == QUOTA_EXCEEDED_ERR) {
-            alert('Quota exceeded!'); //data wasn’t successfully saved due to quota exceed so throw an error
-          }
-        }
-        //document.write(localStorage.getItem(“name”)); //Hello World!
-        // localStorage.removeItem(“name”); //deletes the matching item from the database
-      }
-
-    });
-
-    console.log("redirect");
-    for (var i = 0; i < localStorage.length; i++) {
-      var key = localStorage.key(i);
-      if (key.indexOf('jdhelperurl_') > -1) {
-        window.location.href = localStorage.getItem(key);
-        break;
-      }
+    if ($("a[class='btn-def']:contains('评价')") != undefined) {
+      $("a[class='btn-def']:contains('评价')")[0].click();
     }
-
-
-
   }
 
   if (document.URL.indexOf("orderVoucher.action") > -1) {
+
+
     console.log("auto commments star");
     console.log($('span[data-id="A1"]'));
     $('span[data-id="A1"]').each(function () {
@@ -70,12 +37,17 @@ function myMain(evt) {
 
     $('textarea[placeholder="商品是否给力？快分享你的购买心得吧~"]').val("东西挺好的值得购买，下次还会再来。");
 
-    $("a[class='btn-submit']").click();
+
+    $('a[class="btn-submit"]')[0].click();
+
+
+
 
   }
-  if(document.URL.indexOf("saveCommentSuccess.action") > -1)
-  {
-      window.location.href = "https://club.jd.com/myJdcomments/myJdcomment.action";
+  if (document.URL.indexOf("saveCommentSuccess.action") > -1) {
+    window.location.href = "https://club.jd.com/myJdcomments/myJdcomment.action";
   }
+
+
 
 }
